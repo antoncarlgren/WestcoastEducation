2a154b67f0739978b4456f651c77f26b7a567ba2
Anton Carlgren <anton.carlgren@hotmail.com>
2022-05-26 15:30:28 +0200

    Start working on React admin application. Add navbar.

---
+++ Clients/react-apps/admin-app/src/components/courses/CourseItem.jsx
@@ -0,0 +1,35 @@
+﻿import { useNavigate } from 'react-router-dom';
+
+const CourseItem = ({course, handleDeleteCourse}) => {
+    const navigate = useNavigate();
+    
+    const onEditClickHandler = () => {
+      navigate(`/edit/${course.courseNo}`);  
+    };
+    
+    const onDeleteClickHandler = () => {
+      handleDeleteCourse(course.courseNo);  
+    };
+    
+    return (
+        <tr>
+            <td>
+                <span onClick={onEditClickHandler}>
+                    <i>
+                        <i className='fa-solid fa-pencil edit'></i>
+                    </i>
+                </span>
+            </td>
+            <td>{course.courseNo}</td>
+            <td>{course.title}</td>
+            <td>{course.length}</td>
+            <td>
+                <span>
+                    <i className='fa-solid fa-trash-can delete'></i>
+                </span>
+            </td>
+        </tr>
+    );
+}
+
+export default CourseItem;
---
+++ Clients/react-apps/admin-app/src/components/courses/CourseList.jsx
@@ -0,0 +1,72 @@
+﻿import { useEffect, useState } from 'react';
+import CourseItem from './CourseItem';
+
+const CourseList = () => {
+    const [courses, setCourses] = useState([]);
+    
+    useEffect(() => {
+       loadCourses();
+    }, []);
+    
+    const loadCourses = async () => {
+        const token = JSON.parse(localStorage.getItem('token'));
+        console.log(token);
+        
+        const url = `${process.env.REACT_APP_BASEURL}/courses/list`;
+
+        const response = await fetch(url, {
+            method: `GET`,
+            headers: {
+                Authorization: `bearer ${token}`
+            }
+        });
+        
+        if(!response.ok) {
+            console.log('Something went wrong while fetching courses.');
+        } else {
+            setCourses(await response.json());
+        }
+    }
+    
+    const deleteCourse = async (id) => {
+        console.log(`Removing course with id ${id}.`);
+        const url = `${process.env.REACT_APP_BASEURL}/courses/${id}`;
+        
+        const response = await fetch(url, {
+            method: 'DELETE'
+        })
+        
+        console.log(response);
+        
+        if(response.status >= 200 && response.status <= 299) {
+            console.log('Course removed.');
+        } else {
+            console.log('Something went wrong while removing course.');
+        }
+    }
+    
+    return(
+        <table>
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Number</th>
+                    <th>Name</th>
+                    <th>Length</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {courses.map((course) => (
+                    <CourseItem
+                        course={course}
+                        key={course.Id}
+                        handleDeleteCourse={deleteCourse}
+                    />    
+                ))}
+            </tbody>
+        </table>
+    );
+}
+
+export default CourseList;
